ffe2b08139dd66b502cd04f9c25932b259a9ddc3
IvanGoMa <99ivangm@gmail.com>
2026-05-29 18:17:51 +0200

    disabled spring security

--- wlbalancer/src/main/java/ivangoma/wlbalancer/config/SecurityConfig.java
+++ wlbalancer/src/main/java/ivangoma/wlbalancer/config/SecurityConfig.java
@@ -3,25 +3,17 @@ package ivangoma.wlbalancer.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-            .csrf(csrf -> csrf.disable())
-            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(
-                            "/swagger-ui/**",
-                            "/v3/api-docs/**",
-                            "/h2-console/**"
-                    ).permitAll()
-                    .anyRequest().authenticated()
-            )
-            .build();
-}
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+        .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 
 }
